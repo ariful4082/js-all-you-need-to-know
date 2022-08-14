a0bb98826afea81a30e6d3bad909138d67340de1
ariful4082 <ariful4082@gmail.com>
2022-08-14 09:09:45 +0600

    simple operation is on this chapter three

---
+++ chapter_three/logicalAndOr.js
@@ -0,0 +1,9 @@
+var name = '';
+
+var fullname = name || 'Ariful Islam';
+
+console.log(fullname);
+
+var isOk = false;
+
+isOk && console.log('Everything is OK')
---
+++ chapter_three/ternary.js
@@ -0,0 +1,17 @@
+var n = 10;
+var str = '';
+
+
+if(n % 2 === 0){
+    str = 'EVEN'
+}else{
+    str = 'ODD'
+}
+
+console.log(str);
+
+var result = n % 2 === 0 ?'Even Number':'Odd Number';
+
+console.log(result);
+
+// condition ? true side : false side

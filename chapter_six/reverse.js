bb51aaadee7a99f9023ae9c5710fce4d139ec0e1
ariful4082 <ariful4082@gmail.com>
2022-08-18 08:26:44 +0600

    array travers, insert, remove, search and reverse

---
+++ chapter_six/reverse.js
@@ -0,0 +1,26 @@
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+var result = []
+
+
+// for(var i =0;i<num.length; i++){
+//     result.unshift(num[i])
+// }
+
+// console.log(result);
+
+// for(var i = num.length-1; i >= 0; i--){
+//     result.push(num[i])
+// }
+
+// console.log(result);
+
+
+// for(var i = 0; i<arr.length/2; i++){
+//     var temp = arr[i];
+
+//     arr[i]=arr[arr.length-1-i]
+//     arr[arr.length-1-i]=temp
+// }
+
+console.log(arr.reverse());
---
+++ chapter_six/search.js
@@ -0,0 +1,17 @@
+var arr = [4, 7, 2, 6, 8, 4, 55, 9, 89, 27];
+
+var find = 55;
+var isFound = false;
+
+
+for(var i =0; i<arr.length;i++){
+    if(arr[i]=== find){
+        console.log('Data Found at Index '+ i);
+        isFound=true
+        break
+    }
+}
+
+if(!isFound){
+    console.log('Data Not Found');
+}

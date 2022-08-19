bc5eb8fb0f177d0e54f5b1e53cece0bf3c76f451
ariful4082 <ariful4082@gmail.com>
2022-08-19 06:37:20 +0600

    basic array is done

---
+++ chapter_six/methods.js
@@ -0,0 +1,26 @@
+// var arr = [4, 5, 9, 6];
+// var arr2 = [5, 10, 45, 23];
+
+// console.log(arr.join(' | '))
+
+// arr.fill(0);
+// console.log(arr)
+
+// var arr3 = arr.concat(arr2)
+
+// console.log(arr3);
+
+// console.log(Array.isArray(arr))
+
+// var arr4 = Array.from(arr)
+// console.log(arr);
+
+var a = [1,2];
+
+var b = Array.from(a)
+
+
+b[0]=5
+
+console.log(a);
+console.log(b);

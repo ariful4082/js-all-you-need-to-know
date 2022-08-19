86374d8e6c64fa02ba9c8f20f4ea46a7e9c4072f
ariful4082 <ariful4082@gmail.com>
2022-08-19 14:32:32 +0600

    create object literal vs constractor formate

---
+++ chapter_seven/object.js
@@ -0,0 +1,59 @@
+// student 1:
+//Math:80
+// chemistry:75
+// physics: 86
+// higher Math: 90
+
+// [80, 75, 86, 90]
+
+// var obj = {};
+// obj.x = 10
+
+// console.log(typeof obj);
+// console.log( obj);
+
+
+// var point = {
+//     x: 10,
+//     y: 20
+// }
+// // point.y = 30
+// point.z = 30;
+
+
+
+// console.log(point);
+
+// var obj2 = new Object();
+// obj2.b = 20;
+
+// console.log(obj2)
+
+
+//*************** Accessing Object Property */
+
+var point = {
+    x:10,
+    y: 20,
+    z: 15
+}
+// console.log(point.y);
+// console.log(point.y)
+
+// console.log(point.x+point.z)
+
+// console.log(point['x'])
+// console.log(point['z'])
+
+// var show ='z'
+
+// console.log(point[show]);
+
+
+point.x = 45;
+point.y = 16;
+var prop = 55;
+// console.log(point)
+
+
+console.log(point)

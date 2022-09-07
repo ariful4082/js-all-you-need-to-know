cb4416d03c4a35d730e86a0daf91647ea438e7e6
ariful4082 <ariful4082@gmail.com>
2022-09-07 07:14:29 +0600

    create input and edit date then replace exiting data

--- chapter_eighteens/src/lessons/lesson17.js
+++ chapter_eighteens/src/lessons/lesson17.js
@@ -5,3 +5,64 @@ name.addEventListener('keypress', function(e){
         
     }
 })
+
+
+let skills = document.getElementsByName('skills');
+let result = document.getElementById('result')
+
+let checkedSkills = [];
+
+
+[...skills].forEach(skill=>{
+    skill.addEventListener('change', function(e){
+        if(e.target.checked){
+            checkedSkills.push(e.target.value)
+            outputSkills(result, checkedSkills)
+            
+        }else{
+            let ind = checkedSkills.indexOf(e.target.value)
+            checkedSkills.splice(ind,1)
+            outputSkills(result, checkedSkills)
+            
+        }
+    })
+})
+
+function outputSkills(parent, skills){
+    let result = ''
+    skills.forEach((skill, index)=>{
+        result+=` (${index+1}) ${skill} `
+    })
+    parent.innerHTML = result
+}
+
+
+
+let list = document.getElementById('list')
+list.addEventListener('dblclick', function(e){
+    if(this.contains(e.target)){
+        let innerText = e.target.innerText
+        e.target.innerHTML = ''
+        let inputBox = createInputBox(innerText)
+        e.target.appendChild(inputBox)
+
+        inputBox.addEventListener('keypress', function(event){
+            if(event.key === 'Enter'){
+                e.target.innerHTML = event.target.value
+            }
+        })
+    }
+})
+
+function createInputBox(value){
+    let inp = document.createElement('input')
+    inp.type = 'text'
+    inp.className = 'form-control'
+    inp.value = value
+
+    return inp
+}
+
+// list.addEventListener('click', function(e){
+//     console.log('Click')
+// })
